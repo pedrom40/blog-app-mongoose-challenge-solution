e7395b3de1db4cc1210f811f85d837cd073af8b5
pedrom40 <pedro.morin1980@gmail.com>
2017-10-20 08:51:37 -0500

    setup test dir and init test file

---
+++ test/test-blog-integration.js
@@ -0,0 +1,13 @@
+// necessary libraries and settings
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const should = chai.should();
+
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
