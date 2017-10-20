83bb3faaa8bb28cd429fe6fedfbea7efe5f673ae
pedrom40 <pedro.morin1980@gmail.com>
2017-10-20 09:11:33 -0500

    added main resource test that generates then deletes test data

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -11,3 +11,61 @@ const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
+
+
+// seed db with test data
+function seedData () {
+  console.log('seeding test data');
+
+  // var to hold test data
+  const seedData = [];
+
+  // insert 10 records
+  for (let i=0; i < 10; i++) {
+    seedData.push(generateData());
+  }
+
+  // return the test data
+  return BlogPost.insertMany(seedData);
+
+}
+
+// generate test data
+function generateData () {
+  return {
+    author: {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName()
+    },
+    title: faker.lorem.sentence(),
+    content: faker.lorem.paragraphs(),
+    created: faker.date.recent()
+  }
+}
+
+// delete test data
+function tearDownDb () {
+  console.warn('deleting test db');
+  return mongoose.connection.dropDatabase();
+}
+
+
+describe('Blog Posts API resource', () => {
+
+  before( () => {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach( () => {
+    return seedData();
+  });
+
+  afterEach( () => {
+    return tearDownDb();
+  });
+
+  after( () => {
+    return closeServer();
+  });
+
+});
