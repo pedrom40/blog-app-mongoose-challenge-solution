c71d6fe9d62365cc8d2c7dd36a0aa5bb60464216
pedrom40 <pedro.morin1980@gmail.com>
2017-10-23 08:51:17 -0500

    add put and delete tests, all passing now

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -103,4 +103,123 @@ describe('Blog Posts API resource', () => {
 
   });
 
+  // test POST endpoint
+  describe('POST endpoint', () => {
+
+    it('should add a new post', () => {
+
+      // init new record
+      const newRecord = generateData();
+
+      return chai.request(app)
+        .post('/posts')
+        .send(newRecord)
+        .then( res => {
+
+          // ensure response has all necessary info
+          expect(res).to.have.status(201);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.deep.keys({ id: 1, author: { firstName: 1, lastName: 1 }, title: 1, content: 1, created: 1 });
+
+          // check that the info matches what was passed in
+          expect(res.body.author).to.equal(`${newRecord.author.firstName} ${newRecord.author.lastName}`);
+          expect(res.body.title).to.equal(newRecord.title);
+          expect(res.body.content).to.equal(newRecord.content);
+
+          // pass new record to next part in the chain (promise)
+          return BlogPost.findById(res.body.id);
+
+        })
+        .then( post => {
+
+          // check that the title in the post matches the title in the db
+          expect(post.title).to.equal(newRecord.title);
+
+        });
+
+    });
+
+  });
+
+  // test PUT endpoint
+  describe('PUT endpoint', () => {
+
+    it('should update the fields you send over', () => {
+
+      // data to update
+      const updateData = {
+        title: 'Updated Title',
+        content: 'Updated content'
+      }
+
+      return BlogPost
+        .findOne()
+        .then( post => {
+
+          // set the id property of the update data to the record we returned
+          updateData.id = post.id;
+
+          // make the update, then return the record to the next promise
+          return chai.request(app)
+            .put(`/posts/${post.id}`)
+            .send(updateData);
+
+        })
+        .then( res => {
+
+          // check status
+          expect(res.status).to.equal(204);
+
+          // get the record
+          return BlogPost.findById(updateData.id);
+
+        })
+        .then( post => {
+
+          // check that the title and content equal the updated values
+          expect(post.title).to.equal(updateData.title);
+          expect(post.content).to.equal(updateData.content);
+
+        });
+
+    });
+
+  });
+
+  // test DELETE endpoint
+  describe('DELETE endpoint', () => {
+
+    it('should delete a record by id', () => {
+
+      // declare post var here so we can access it across the test
+      let post;
+
+      return BlogPost
+        .findOne()
+        .then ( _post => {
+
+          // set the returned post as our test var, then send for deletion
+          post = _post;
+          return chai.request(app).delete(`/posts/${post.id}`);
+
+        })
+        .then( res => {
+
+          // check for correct stats
+          expect(res.status).to.equal(204);
+
+          // attempt to retrieve the record
+          return BlogPost.findById(post.id);
+
+        })
+        .then( _post => {
+
+          // should not find record
+          expect(_post).to.not.exist;
+
+        });
+    });
+
+  });
+
 });
